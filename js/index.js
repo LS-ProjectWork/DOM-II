acc74ed4af46d4656205a99a20336d58cd8ac606
dpayton23 <darien.payton71@gmail.com>
2019-03-05 17:54:41 -0500

    added event listener to footer

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const colorChange = document.querySelector(".footer");
+colorChange.addEventListener("mouseover", function(e) {
+    e.target.style.color = "crimson";
+})
+console.log(colorChange);
+
