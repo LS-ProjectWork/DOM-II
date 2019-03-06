26f87f4e0a700a2d367bd0112e12407e9d6b6daa
dpayton23 <darien.payton71@gmail.com>
2019-03-06 01:12:35 -0500

    stopped propogation from window resizing (using keyboard shortcuts) to keydown

--- js/index.js
+++ js/index.js
@@ -11,6 +11,11 @@ btn.forEach(function(currentValue) {
     });
 });
 
+const allText = document.querySelector("body");
+allText.addEventListener("copy", function(event) {
+    event.target.style.color = "yellow";
+})
+
 const pageContent = document.querySelectorAll("p");
 pageContent.forEach(function(currentValue) {
     currentValue.addEventListener("click", function(e) {
@@ -18,21 +23,17 @@ pageContent.forEach(function(currentValue) {
     });
 });
 
-window.addEventListener("keypress", function() {
-    // console.log("Someone pressed a key!");
+window.addEventListener("keydown", function() {
+    console.log("Someone pressed a key!");
 });
 
 window.addEventListener("load", function() {
     console.log("The resources have finished loading.")
 });
 
-const allText = document.querySelector("body");
-allText.addEventListener("copy", function(event) {
-    event.target.style.color = "yellow";
-})
-
-window.addEventListener("resize", function() {
-    alert("the window size has changed.")
+window.addEventListener("resize", function(event) {
+    event.stopPropagation()
+    console.log("the window size has changed.")
 })
 
 window.addEventListener("wheel", function(e) {
